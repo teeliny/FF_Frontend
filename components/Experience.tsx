20480957c101807be8ce932e83c36a95d5402258
teeliny <teeliny@gmail.com>
2023-10-27 17:12:45 +0100

    used png for wishes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -85,24 +85,27 @@ export const Experience = () => {
             </div>
 
             {!realityMode && !submitWishModal && (
-              <div className='fixed bottom-0 left-0 flex flex-col w-full pb-4 mt-6' style={{ background: "rgba(10, 48, 133, 0.70)" }}>
-                <div className='w-full' style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 200px)', overflow: 'scroll' }}>
-                  {realityList.map((wish) => (
-                      <div key={wish.id} style={{width: '200px'}} className='!mr-0 !flex flex-col gap-2 items-center' onClick={() => slideChangeHandler(wish.id)}>
-                        <Image src={wish.img} width={10} height={8} alt={`wish-${wish.id}`} style={{width: '200px'}} className={`w-48 max-h-28 ${currSlide === wish.id ? 'border-2 border-yellow-300' : ''}`} />
-                        <p className='mx-2 text-sm font-semibold text-center text-white'>{wish.description}</p>
-                      </div>
-                    ))}
+              <Fragment>
+                <div className='fixed top-0 left-0 w-full h-full' style={{background: "linear-gradient(180deg, rgba(10, 48, 133, 0.00) 31.56%, #0A3085 75.26%)"}}></div>
+                <div className='fixed bottom-0 left-0 flex flex-col w-full pb-4 mt-6'>
+                  <div className='w-full' style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 200px)', overflow: 'scroll' }}>
+                    {realityList.map((wish) => (
+                        <div key={wish.id} style={{width: '200px'}} className='!mr-0 !flex flex-col gap-2 items-center' onClick={() => slideChangeHandler(wish.id)}>
+                          <Image src={wish.img} width={10} height={8} alt={`wish-${wish.id}`} style={{width: '200px'}} className={`w-48 max-h-28 ${currSlide === wish.id ? 'border-2 border-yellow-300' : ''}`} />
+                          <p className='mx-2 text-sm font-semibold text-center text-white'>{wish.description}</p>
+                        </div>
+                      ))}
+                  </div>
+                  <button
+                    style={{color: +currSlide >= 1 ? '#0A3085' : '#1A191999', backgroundColor: +currSlide >= 1 ? '#FFFF00' : '#636463'}}
+                    className={`self-center py-3 mt-4 uppercase px-14 w-fit`} 
+                    onClick={submitWishHandler} 
+                    disabled={+currSlide < 1}
+                  >
+                    make my wish
+                  </button>
                 </div>
-                <button
-                  style={{color: +currSlide >= 1 ? '#0A3085' : '#1A191999', backgroundColor: +currSlide >= 1 ? '#FFFF00' : '#636463'}}
-                  className={`self-center py-3 mt-4 uppercase px-14 w-fit`} 
-                  onClick={submitWishHandler} 
-                  disabled={+currSlide < 1}
-                >
-                  make my wish
-                </button>
-              </div>
+              </Fragment>
             )}
           </div>
 
